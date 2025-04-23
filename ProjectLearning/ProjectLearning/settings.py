7c4d2625af3c23667f6e5814602ad3cb689ab200
unknown <yornamerator@yandex.ru>
2025-04-23 22:22:42 +0400

    Обновление проекта: добавлены шаблоны, исправлен путь templates и др.

--- ProjectLearning/ProjectLearning/settings.py
+++ ProjectLearning/ProjectLearning/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3uuu(l%uy^7v!79)wij8em1esrjon4s0@m^024n*@b)27z7+w#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'ProjectLearning.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'D:/Рабочий стол/Быстрый доступ/документы/Диплом/ProjectLearning/ProjectLearning/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
